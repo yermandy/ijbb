5d69e10a5725ef531f7468f0c7212eaee4ab6bb8
“yermaand” <yermaand@fel.cvut.cz>
2020-04-10 11:09:36 +0200

    ✨yaw and roll covariates

--- covariates/cov_generate.py
+++ covariates/cov_generate.py
@@ -31,11 +31,55 @@ def create_pairs(from_identities):
 
     return np.concatenate((pairs, labels[:, np.newaxis]), axis=1)
 
-def save_pairs(cov, min_len, name):
-    cov = np.random.choice(cov, min_len, replace=False)
-    cov_pairs = create_pairs(cov)
+def create_pairs_cont(from_identities, cov, val_from, val_to):
+    pairs = combinations(from_identities, 2)
+    pairs = [*pairs]
+    pairs = np.array(pairs)
+
+    t1_i = pairs[:, 0]
+    t2_i = pairs[:, 1]
+
+    diffs = abs(cov[t1_i] - cov[t2_i])
+    diff_mask = np.where((diffs >= val_from) & (diffs < val_to))
+
+    t1_i = t1_i[diff_mask]
+    t2_i = t2_i[diff_mask]
+    pairs = pairs[diff_mask]
+
+    identities = faces[:, 6]
+
+    labels = np.where(identities[t1_i] == identities[t2_i], 1, 0)
+
+    impostor = len(np.flatnonzero(labels == 0))
+    genuine = len(labels) - impostor
+    print('Impostor: ', impostor)
+    print('Genuine: ', genuine)
+
+    for i, (f1, f2) in enumerate(pairs):
+        pairs[i] = [faces_templates[f1], faces_templates[f2]]
+
+    # size = len(pairs) if len(pairs) < 3000000 else 3000000
+
+    # genuine_pairs = np.flatnonzero(labels == 1)
+    # random_pairs = np.random.choice(pairs.shape[0], size - genuine) # todo delete
+    # random_pairs = np.concatenate((random_pairs, genuine_pairs))
+    # pairs = pairs[random_pairs]
+    # labels = labels[random_pairs]
+    # print('Test set: ', len(labels))
+    return np.concatenate((pairs, labels[:, np.newaxis]), axis=1)
+
+
+def save_pairs(cov_ids, min_len, name):
+    min_len = len(cov_ids) if min_len > len(cov_ids) else min_len
+    cov_ids = np.random.choice(cov_ids, min_len, replace=False)
+    cov_pairs = create_pairs(cov_ids)
     np.savez_compressed(f'resources/covariates/{name}.npz', comparisons=cov_pairs)
 
+def save_pairs_cont(cov_ids, cov, val_from, val_to, min_len, name):
+    min_len = len(cov_ids) if min_len > len(cov_ids) else min_len
+    cov_ids = np.random.choice(cov_ids, min_len, replace=False)
+    cov_pairs = create_pairs_cont(cov_ids, cov, val_from, val_to)
+    np.savez_compressed(f'resources/covariates/{name}.npz', comparisons=cov_pairs)
 
 
 if __name__ == "__main__":
@@ -51,7 +95,7 @@ if __name__ == "__main__":
 
     save_pairs(cov_0, min_len, 'cov_forehead_0')
     save_pairs(cov_1, min_len, 'cov_forehead_1')
-    '''
+    # '''
     
     # NOSE_MOUTH_VISIBLE
     '''
@@ -64,7 +108,7 @@ if __name__ == "__main__":
 
     save_pairs(cov_0, min_len, 'cov_nose_mouth_0')
     save_pairs(cov_1, min_len, 'cov_nose_mouth_1')
-    '''
+    # '''
 
     # FACIAL_HAIR
     '''
@@ -81,4 +125,50 @@ if __name__ == "__main__":
     save_pairs(cov_1, min_len, 'cov_facial_hair_1')
     save_pairs(cov_2, min_len, 'cov_facial_hair_2')
     save_pairs(cov_3, min_len, 'cov_facial_hair_3')
+    # '''
+    
+    # YAW
     '''
+    cov = metadata[:, 8].astype(np.float32)
+    
+    cov = np.abs(cov)
+
+    with np.errstate(invalid='ignore'):
+        cov_0 = np.flatnonzero((cov >= 0) & (cov < 15))
+        cov_1 = np.flatnonzero((cov >= 15) & (cov < 30))
+        cov_2 = np.flatnonzero((cov >= 30) & (cov < 45))
+        cov_3 = np.flatnonzero((cov >= 45) & (cov < 90))
+
+    min_len = np.min([len(cov_0), len(cov_1), len(cov_2), len(cov_3)])
+    # print(min_len)
+    print(len(cov_0))
+    print(len(cov_1))
+    print(len(cov_2))
+    print(len(cov_3))
+    min_len = 5500
+    # raise Exception
+
+    save_pairs_cont(cov_0, cov, 0, 15, min_len, 'cov_yaw_0_15')
+    save_pairs_cont(cov_1, cov, 10, 35, min_len, 'cov_yaw_15_30')
+    save_pairs_cont(cov_2, cov, 12.5, 60, min_len, 'cov_yaw_30_45')
+    save_pairs_cont(cov_3, cov, 20, 90, min_len, 'cov_yaw_45_90')
+    # '''
+
+    # ROLL
+    '''
+    cov = metadata[:, 9].astype(np.float32)
+    
+    abs_cov = np.abs(cov)
+
+    with np.errstate(invalid='ignore'):
+        cov_0 = np.flatnonzero((abs_cov >= 0) & (abs_cov < 15))
+        cov_1 = np.flatnonzero((abs_cov >= 15) & (abs_cov < 65))
+
+    min_len = np.min([len(cov_0), len(cov_1)])
+    print(len(cov_0))
+    print(len(cov_1))
+    min_len = 4000
+
+    save_pairs_cont(cov_0, cov, 0, 15, min_len, 'cov_roll_0_15')
+    save_pairs_cont(cov_1, cov, 15, 65, min_len, 'cov_roll_15_65')
+    # '''
--- covariates/cov_plot.py
+++ covariates/cov_plot.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_coviariates(qualities, labels, fig_name, save=True, show=True, fill=True, histtype='bar', bins=30, alpha=0.75):
+def plot_coviariates(qualities, labels, fig_name, save=True, show=True, fill=True, histtype='bar', bins=40, alpha=0.75):
     fig, axs = plt.subplots(1, 1, tight_layout=True)
 
     for i, quality in enumerate(qualities):
@@ -33,13 +33,26 @@ def separate_qualities(cov, labels, quality):
 
     return qualities
 
+def separate_qualities_cont(cov, labels, quality):
+    not_nan = np.flatnonzero(cov != 'NaN')
+
+    quality = quality[not_nan]
+    cov = cov[not_nan].astype(np.float32)
+    cov = np.abs(cov)
+
+    qualities = []
+    for label in labels:
+        cov_i = np.flatnonzero((cov >= label[0]) & (cov < label[1]))
+        qualities.append(quality[cov_i])
+
+    return qualities
 
 
 if __name__ == "__main__":
     
     metadata = np.genfromtxt(f"resources/ijbb_faces_cov.csv", dtype=np.str, delimiter=',', skip_header=1)
     # quality = np.load('resources/features/norms_retina_ijbb_0.5.npy')
-    quality = np.load('resources/qualities/ijbb_cnn_fq_score_model_24.npy')
+    quality = np.load('resources/qualities/ijbb_cnn_fq_score_model_31.npy')
 
     if len(np.flatnonzero(quality > 1)) > 0:
         min_qs = np.min(quality)
@@ -47,18 +60,42 @@ if __name__ == "__main__":
         quality = (quality - min_qs) / (max_qs - min_qs)
 
 
+    '''
     # Forehead
     cov = metadata[:, 2] 
     qualities = separate_qualities(cov, ['1', '0'], quality)
     plot_coviariates(qualities, ['Forehead visible = 1', 'Forehead visible = 0'], 'cov_forehead', show=False)
+    # '''
 
     # Nose / mouth 
+    '''
     cov = metadata[:, 1] 
     qualities = separate_qualities(cov, ['1', '0'], quality)
     plot_coviariates(qualities, ['Nose/mouth visible = 1', 'Nose/mouth visible = 0'], 'cov_nose_mouth', show=False)
+    # '''
 
     # Facial hair
+    '''
     cov = metadata[:, 3] 
     qualities = separate_qualities(cov, ['0', '1', '2', '3'], quality)
     labels = ['Facial hair = 0', 'Facial hair = 1', 'Facial hair = 2', 'Facial hair = 3']
-    plot_coviariates(qualities, labels, 'cov_facial_hair', show=False, histtype='step', fill=False)
+    plot_coviariates(qualities, labels, 'cov_facial_hair', show=False)
+    # '''
+
+    # Yaw
+    '''
+    cov = metadata[:, 8]
+    labels = [(0, 15), (15, 30), (30, 45), (45, 90)]
+    qualities = separate_qualities_cont(cov, labels, quality)
+    labels = ['Yaw [0°, 15°]', 'Yaw [15°, 30°]', 'Yaw [30°, 45°]', 'Yaw [45°, 90°]']
+    plot_coviariates(qualities, labels, 'cov_yaw', show=False)
+    # '''
+
+    # Roll
+    '''
+    cov = metadata[:, 9]
+    labels = [(0, 15), (15, 65)]
+    qualities = separate_qualities_cont(cov, labels, quality)
+    labels = ['Roll [0°, 15°]', 'Roll [15°, 65°]']
+    plot_coviariates(qualities, labels, 'cov_roll', show=False, bins=30)
+    # '''
